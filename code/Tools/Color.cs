89d12270da49aec3406646ab327de156d06dd54d
Ryhon <ryhon@ryhn.link>
2021-08-17 13:47:36 +0200

    Color tool options

--- code/Tools/Color.cs
+++ code/Tools/Color.cs
@@ -1,8 +1,14 @@
-﻿namespace Sandbox.Tools
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Sandbox.Tools
 {
 	[Library( "tool_color", Title = "Color", Description = "Change render color and alpha of entities", Group = "construction" )]
 	public partial class ColorTool : BaseTool
 	{
+		[ConVar.ClientData( "tool_color_tint" )]
+		public static uint Tint { get; set; }
+
 		public override void Simulate()
 		{
 			if ( !Host.IsServer )
@@ -10,8 +16,10 @@
 
 			using ( Prediction.Off() )
 			{
-				if ( Input.Pressed( InputButton.Attack1 ) || Input.Pressed( InputButton.Attack2 ) )
+				if ( Input.Pressed( InputButton.Attack1 ) || Input.Pressed( InputButton.Reload ) )
 				{
+					var clear = Input.Pressed( InputButton.Reload );
+
 					var startPos = Owner.EyePos;
 					var dir = Owner.EyeRot.Forward;
 
@@ -27,14 +35,37 @@
 					if ( tr.Entity is not ModelEntity modelEnt )
 						return;
 
-					modelEnt.RenderColor =
-						Input.Pressed( InputButton.Attack2 )
-						? (Owner as SandboxPlayer).PlayerColor
-						: Color.Random.ToColor32();
+					modelEnt.RenderColor = clear ? Color.White : this.GetColorConvar( "tool_color_tint", Color.White );
 
 					CreateHitEffects( tr.EndPos );
 				}
 			}
+		}
+
+		public override Panel CreatePanel()
+			=> new ColorToolPanel( this );
+	}
+
+
+	class ColorToolPanel : Panel
+	{
+		public ColorToolPanel( ColorTool t )
+		{
+			StyleSheet.Load( "/Tools/Color.scss" );
+			Add.Label( "Color", "title" );
+
+			var color = new ColorPicker();
+			AddChild( color );
+
+			var s = ConsoleSystem.GetValue( "tool_color_tint", Color.Red.RGBA.ToString() );
+			uint rgba = 0;
+			uint.TryParse( s, out rgba );
+			color.Value = new Color( rgba );
+
+			color.AddEventListener( "OnValueChanged", () =>
+			{
+				ConsoleSystem.Run( "tool_color_tint", color.Value.ToColor().RGBA );
+			} );
 		}
 	}
 }
