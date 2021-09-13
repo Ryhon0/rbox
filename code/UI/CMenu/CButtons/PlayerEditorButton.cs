41a1c107d3e465610499576da3d2f2c191811dd0
Ryhon <ryhon@ryhn.link>
2021-09-13 20:24:29 +0200

    CMenu

---
+++ code/UI/CMenu/CButtons/PlayerEditorButton.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[Library( "cb_player", Title = "cb_player" )]
+public class CBPlayer : CButton
+{
+	public override string IconPath => "ui/cmenu/player.png";
+
+	Window current;
+	public CBPlayer() : base()
+	{
+		AddEventListener( "onclick", () =>
+		{
+			if ( current?.Parent == null )
+			{
+				current = Window.With( new PlayerEditor() )
+					.WithSize( 450, 500 )
+					.WithTitle( "Player" )
+					.WithResizable( false );
+
+				CMenu.Current.AddChild( current );
+
+				current.Center();
+			}
+			else current.Delete();
+		} );
+	}
+
+	public override void OnDeleted()
+	{
+		base.OnDeleted();
+		current?.Delete();
+	}
+}
---
+++ code/UI/CMenu/CButtons/SwitchViewButton.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox;
+
+[Library("cb_switch_view", Title = "Switch View")]
+public class SwitchViewButton : CButton
+{
+	public override string IconPath => "ui/cmenu/camera.png";
+	public SwitchViewButton() : base()
+	{
+		AddEventListener( "onclick", () => ConsoleSystem.Run( "switchview" ) );
+	}
+}
